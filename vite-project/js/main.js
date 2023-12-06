c12e2d15901d3f147bd3b4c988b295fd458dd212
maggiey8 <144460257+maggiey8@users.noreply.github.com>
2023-12-06 10:14:57 -0500

    HELP !!!!!!!

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,12 +1,27 @@
 import '../css/style.css'
-import {DOMSelectors} from 'dom.js'
+import {DOMSelectors} from './dom'
 
 function getColors() {
     //smth with api here????
     let main = 'rgb value here (from the sprite) (how do i read the rgb from the sprite'
 }
 
-DOMSelectors.submit.addEventlistener("click", function(event) {
-    event.preventDefault()
+const URL = 'https://pokeapi.co/api/v2/pokemon/ditto'
 
-})
+async function getData(URL) {
+    try {
+        const resonse = await fetch(URL);
+        if (Response.status !=200) {
+            throw new Error(response.statusText)
+        }
+        const data = await response.json();
+        DOMSelectors.submit.addEventlistener("submit", function(event) {
+            event.preventDefault();
+
+        })
+    } catch (error) {
+        console.log(error, "not good")
+    }    
+}
+
+getData();
