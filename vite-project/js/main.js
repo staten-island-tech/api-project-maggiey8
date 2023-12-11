1a6f7a3a30ed813d7d32d7db682c06af13f2c954
maggiey8 <144460257+maggiey8@users.noreply.github.com>
2023-12-11 10:15:29 -0500

    e

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -6,30 +6,16 @@ function getColors() {
     let main = 'rgb value here (from the sprite) (how do i read the rgb from the sprite'
 }
 
-let URL = `https://pokeapi.co/api/v2/pokemon/ditto`
-// /${DOMSelectors.input.value}
-
-async function getData(URL) {
-    try {
-        const response = await fetch(URL);
-/*          if (response.status !=200) {
-            throw new Error(response.statusText)
-        }  */
-        const data = await response.json();
-        document.querySelector('h1').textContent = data.species.name;
-    } catch (error) {
-        console.log(error, "not good")
-    }    
-}
-
-
-async function inject(URL) {
+async function inject(URL, URL1) {
     try {
     let response = await fetch(URL);
     let data = await response.json();
-    console.log(data);       
+    let response1 = await fetch(URL1);
+    let data1 = await response1.json();
+    console.log(data);
+    console.log(data1)       
     document.querySelector('h1').insertAdjacentHTML(
-        'afterbegin', `<div><img src=${data.sprites.front_default} alt=""></div>`
+        'afterbegin', `<div><img src=${data.sprites.front_default} alt=""><h2>${data1.color.name}</h2></div>`
     )
     } catch (error) {
         console.log(error)
@@ -38,8 +24,23 @@ async function inject(URL) {
     
 
 DOMSelectors.submit.addEventListener("click", function(event) {
+    let URL = `https://pokeapi.co/api/v2/pokemon/${DOMSelectors.input.value}`
+    let URL1 = `https://pokeapi.co/api/v2/pokemon-species/${DOMSelectors.input.value}/`
     event.preventDefault();
+    console.log(URL1)
     console.log(URL);
-    inject(URL)
+    inject(URL, URL1)
 }
 )
+
+document.getElementById('eyedropper').addEventListener('click', function() {
+    const resultElement = document.getElementById('result');
+    const eyeDropper = new EyeDropper();
+
+    eyeDropper.open().then((result) => {
+    resultElement.textContent = result.sRGBHex;
+    resultElement.style.backgroundColor = result.sRGBHex;
+})
+})
+
+
