082fcc297770db94d7c6882a3500e4cb543891c8
maggiey8 <yangmaggie2006@gmail.com>
2023-12-11 18:30:21 -0500

    yeah

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,7 +1,8 @@
 const DOMSelectors = { 
     mainBody: document.querySelector("body"),
     submit: document.querySelector("#submit"),
-    input: document.querySelector('input')
+    input: document.querySelector('input'),
+    gallery: document.querySelector('.gallery')
 } 
 
 export {DOMSelectors}
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,11 +1,6 @@
 import '../css/style.css'
 import {DOMSelectors} from './dom'
 
-function getColors() {
-    //smth here????
-    let main = 'rgb value here (from the sprite) (how do i read the rgb from the sprite'
-}
-
 async function inject(URL, URL1) {
     try {
     let response = await fetch(URL);
@@ -14,24 +9,51 @@ async function inject(URL, URL1) {
     let data1 = await response1.json();
     console.log(data);
     console.log(data1)       
-    document.querySelector('h1').insertAdjacentHTML(
-        'afterbegin', `<div><img src=${data.sprites.front_default} alt=""><h2>${data1.color.name}</h2></div>`
+    document.querySelector('.container').insertAdjacentHTML(
+        'beforeend', `<div class="gallery"><img src=${data.sprites.front_default} alt=""><h2>${data1.color.name}</h2></div>`
     )
+    DOMSelectors.input.style.backgroundColor = '#ffffff'
     } catch (error) {
         console.log(error)
+        DOMSelectors.input.value = (error, "This Pokémon doesn't exist")
+        DOMSelectors.input.style.backgroundColor = '#ee242c'
     }  
     }
     
-
-DOMSelectors.submit.addEventListener("click", function(event) {
-    let URL = `https://pokeapi.co/api/v2/pokemon/${DOMSelectors.input.value}`
-    let URL1 = `https://pokeapi.co/api/v2/pokemon-species/${DOMSelectors.input.value}/`
-    event.preventDefault();
+    function URLget() {
+    let URL = `https://pokeapi.co/api/v2/pokemon/${DOMSelectors.input.value.toLowerCase()}`
+    let URL1 = `https://pokeapi.co/api/v2/pokemon-species/${DOMSelectors.input.value.toLowerCase()}/`
     console.log(URL1)
     console.log(URL);
     inject(URL, URL1)
+    }
+
+    function clearFields() {
+        DOMSelectors.input.value = ''
+    }
+
+DOMSelectors.submit.addEventListener("click", function(event) {
+    event.preventDefault();
+    document.querySelector(".container").innerHTML = ""
+    URLget()
+    clearFields()
 }
 )
+
+DOMSelectors.input.addEventListener("keypress", function(event) {
+    if (event.key === 'Enter') {
+        document.querySelector(".container").innerHTML = ""
+        URLget()
+        clearFields()
+    } 
+}
+)
+
+DOMSelectors.input.addEventListener("click", function() {
+    DOMSelectors.input.value =''
+})
+
+
 
 document.getElementById('eyedropper').addEventListener('click', function() {
     const resultElement = document.getElementById('result');
