94b4f55e98e1dad2c869e2d06d7ff93b6ce6c968
maggiey8 <144460257+maggiey8@users.noreply.github.com>
2024-01-03 10:15:27 -0500

    34345

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -9,7 +9,7 @@ function applyTheme(arr) {
     }
 }
 
-function getRGB(arr) { //help how do i use canvases
+/* function getRGB(arr) { //help how do i use canvases
     let canvas = document.getElementById('pokeimage')
     let ctx = canvas.getContext('2d')
     const img = new Image()
@@ -17,25 +17,23 @@ function getRGB(arr) { //help how do i use canvases
         ctx.drawImage(img, 10, 10)
     }
     img.src = `${arr.sprites.front_default}`
-    img.alt = `${arr.name???}`
+    img.alt = `${arr.species.name}`
     console.log(img.src)
     let rgb = ctx.getImageData(0, 0, img.width, img.height);
     console.log(rgb)
-}
+} */
 
 async function inject(URL, URL1) {
     try {
     let response = await fetch(URL);
     let data = await response.json();
     let response1 = await fetch(URL1);
-    let data1 = await response1.json();
-    console.log(data);
-    console.log(data1)       
+    let data1 = await response1.json();     
     document.querySelector('.container').insertAdjacentHTML(
-        'beforeend', `<div class="gallery"><canvas id='pokeimage'></canvas><h2>${data1.color.name}</h2></div>`
+        'beforeend', `<div class="gallery"><img id='pokeimage' src='${data.sprites.front_default}' alt='${data.species.name}'></img><h2>${data1.color.name}</h2></div>`
     )
     applyTheme(data1)
-    getRGB(data)
+/*     getRGB(data) */
     } catch (error) {
         console.log(error)
         DOMSelectors.input.value = ("This Pokémon doesn't exist")
@@ -77,24 +75,35 @@ DOMSelectors.input.addEventListener("keypress", function(event) {
 }
 )
 
-DOMSelectors.input.addEventListener("click", function() {
+/* DOMSelectors.input.addEventListener("click", function() {
     clearFields()
     }
 )
+ */
 
-document.getElementById('eyedropper').addEventListener('click', function() {
-    const resultElement = document.getElementById('result');
+document.getElementById('main').addEventListener('click', function() {
     const eyeDropper = new EyeDropper();
-
+    const button = document.getElementById('main')
     eyeDropper.open().then((result) => {
-    resultElement.textContent = result.sRGBHex;
-    resultElement.style.backgroundColor = result.sRGBHex;
+    console.log(result)
+    button.textContent = result.sRGBHex;
+    //button.style.color =  
+    button.style.backgroundColor = result.sRGBHex;
 })
 })
 
-document.querySelectorAll('button').addEventListener('click', function(event) {
+document.getElementById('secondary').addEventListener('click', function() {
     const eyeDropper = new EyeDropper();
-    let button = (event.target);
+    const button = document.getElementById('secondary')
+    eyeDropper.open().then((result) => {
+    button.textContent = result.sRGBHex;
+    button.style.backgroundColor = result.sRGBHex;
+})
+})
+
+document.getElementById('accent').addEventListener('click', function() {
+    const eyeDropper = new EyeDropper();
+    const button = document.getElementById('accent')
     eyeDropper.open().then((result) => {
     button.textContent = result.sRGBHex;
     button.style.backgroundColor = result.sRGBHex;
